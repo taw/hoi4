e5d6d960cacc08c9bc1b0c8e373dd46235108394
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-07-22 20:26:19 +0100

    Start some calculations in Country

--- src/database/Country.js
+++ src/database/Country.js
@@ -5,7 +5,40 @@ export default class Country {
   }
 
   availableUnits() {
-    // raise "NOPE :-(";
+    // FIXME: actually calculate
     return ["Infantry", "Artillery", "Medium Tanks"];
+  }
+
+  // PRIVATE
+  enabledSubunits() {
+    let {technologies} = this;
+    let result = new Set();
+    for(let tech of technologies) {
+      for(let name of (tech.enable_subunits || [])) {
+        result.add(name);
+      }
+    }
+    return result;
+  }
+
+  enabledEquipments() {
+    let {technologies} = this;
+    let result = new Set();
+    for(let tech of technologies) {
+      for(let name of (tech.enable_equipments || [])) {
+        result.add(name);
+      }
+    }
+    return result;
+  }
+
+  enabledEquipmentArchetypes() {
+    let {db} = this;
+    let result = new Set();
+    for(let eq of this.enabledEquipments()) {
+      let archetype = db.equipment[eq].archetype;
+      result.add(archetype);
+    }
+    return result;
   }
 }
--- src/database/Database.js
+++ src/database/Database.js
@@ -3,9 +3,16 @@ import Country from './Country';
 
 // TODO: support multiple databases so it can work with mods etc.
 export default class Database {
+  constructor() {
+    this.technology = data.technology;
+    this.doctrines = data.doctrines;
+    this.equipment = data.equipment;
+    // this.units = data.units;
+  }
+
   availableYears() {
     let years = new Set();
-    for(let tech of Object.values(data.technology)) {
+    for(let tech of Object.values(this.technology)) {
       let year = tech.start_year;
       if(year) {
         years.add(year);
@@ -15,28 +22,31 @@ export default class Database {
   }
 
   availableDoctrines() {
-    return Object.keys(data.doctrines);
+    return Object.keys(this.doctrines);
   }
 
   technologiesForYear(year) {
     let techs = [];
     if (year) {
-      for (let name in data.technology) {
-        let tech = data.technology[name];
+      for (let name in this.technology) {
+        let tech = this.technology[name];
         if (tech.start_year && tech.start_year < year) {
           techs.push(name);
         }
       }
     }
+    if(!techs.length) {
+      return ["infantry_weapons"];
+    }
     return techs;
   }
 
   technologiesForDoctrine(doctrine) {
-    return data.doctrines[doctrine] || [];
+    return this.doctrines[doctrine] || [];
   }
 
   country(year, doctrine) {
-    let techs = [...this.technologiesForYear(year), this.technologiesForDoctrine(doctrine)];
-    return new Country(this, techs);
+    let techs = [...this.technologiesForYear(year), ...this.technologiesForDoctrine(doctrine)];
+    return new Country(this, techs.map(t => this.technology[t]));
   }
 }
