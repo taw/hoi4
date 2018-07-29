75608791d6c7310fc7213adf6dc4784466a72efa
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-07-29 10:26:00 +0100

    Capitalize terrain names

--- src/components/TerrainRow.js
+++ src/components/TerrainRow.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import TerrainCell from './TerrainCell';
 
+let capitalize = (name) => {
+  return name.charAt(0).toUpperCase() + name.substr(1);
+}
+
 export default class TerrainRow extends Component {
   render() {
     let [name, mov, atk, def] = this.props.data;
     return <tr>
-      <td>{name}</td>
+      <td>{capitalize(name)}</td>
       <TerrainCell data={mov} />
       <TerrainCell data={atk} />
       <TerrainCell data={def} />
