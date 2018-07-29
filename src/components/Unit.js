262e09d31aa0067560a966e1228e807b595453d6
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-07-29 16:23:43 +0100

    Support any number of battallions not just 3

--- src/components/Unit.js
+++ src/components/Unit.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 
 export default class Unit extends Component {
+  normalizeCount = (event) => {
+    let {index, unit, onChange} = this.props;
+    let [count, type] = unit;
+    let normalizedCount = parseInt(count, 10);
+    if(count !== normalizedCount) {
+      onChange(index, [normalizedCount, type]);
+    }
+  }
+
   handleChangeCount = (event) => {
     let count = event.target.value;
+    // A hack to convert once done, but still allow temporary bad values like ""
+    if(count === ""+parseInt(count, 10)) {
+      count = parseInt(count, 10);
+    }
     let {index, unit, onChange} = this.props;
     let type = unit[1];
     onChange(index, [count, type]);
@@ -21,7 +34,7 @@ export default class Unit extends Component {
     let availableFrontline = country.availableUnits().filter(u => u.combat_width > 0).map(u => u.key);
 
     return <div className="unit-select">
-      <input type="number" min="0" value={count} onChange={this.handleChangeCount} />
+      <input type="number" min="0" value={""+count} onChange={this.handleChangeCount} onBlur={this.normalizeCount} />
       <select value={type} onChange={this.handleChangeType}>
         {
           allFrontline.map(unit => <option disabled={availableFrontline.indexOf(unit.key) === -1} key={unit.key} value={unit.key}>{unit.name}</option> )
--- src/components/Units.js
+++ src/components/Units.js
@@ -6,6 +6,18 @@ export default class Units extends Component {
     let {changeUnits, units} = this.props;
     let newUnits = [...units];
     newUnits[index] = unit;
+
+    // Try to maintain just one empty slot
+    while(
+        newUnits.length >= 2 &&
+        newUnits[newUnits.length-1][0] === 0 &&
+        newUnits[newUnits.length-2][0] === 0
+    ) {
+      newUnits.pop();
+    }
+    if(newUnits[newUnits.length-1][0] !== 0) {
+      newUnits.push([0, "infantry"]);
+    }
     changeUnits(newUnits);
   }
 
