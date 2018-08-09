450b46de6d2371113c9a0d818b0648d8b3d51afd
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-08-09 20:06:39 +0100

    Restructure html to avoid random labels

--- src/components/SupportEntry.js
+++ src/components/SupportEntry.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 
 export default class SupportEntry extends Component {
-  constructor(props) {
-    super(props);
-    // Not the best code ever
-    this.id = `support-checkbox-${Math.random()}`;
-  }
   handleChange = (event) => {
     let value = event.target.checked;
     let {data, onSupportChange} = this.props;
@@ -16,8 +11,10 @@ export default class SupportEntry extends Component {
     let {name, available, selected} = data;
     let shortName = name.replace(/^Support /, "").replace(/ Company$/, "");
     return <div className={available ? "checkbox" : "checkbox disabled"} >
-      <input type="checkbox" id={this.id} checked={selected} disabled={!available} onChange={this.handleChange} />
-      <label className="control-label" htmlFor={this.id}>{shortName}</label>
+      <label className="control-label">
+        <input type="checkbox" checked={selected} disabled={!available} onChange={this.handleChange} />
+        {shortName}
+      </label>
     </div>;
   }
 }
