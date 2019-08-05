b1e3e599609f4db726c209209880112dc1e1613f
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-08-05 18:08:09 +0100

    Updates

---
+++ precache-manifest.e28bc9d4d3fee37b8eca185c639d8c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19463b822d0f92889a37abcf3e807b3a",
+    "url": "/hoi4/index.html"
+  },
+  {
+    "revision": "2a63aaba54861bf630c1",
+    "url": "/hoi4/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "fc560cb8460295a7ac8a",
+    "url": "/hoi4/static/css/main.299e863d.chunk.css"
+  },
+  {
+    "revision": "2a63aaba54861bf630c1",
+    "url": "/hoi4/static/js/2.d18fcbbe.chunk.js"
+  },
+  {
+    "revision": "fc560cb8460295a7ac8a",
+    "url": "/hoi4/static/js/main.cea84214.chunk.js"
+  },
+  {
+    "revision": "376cad2d548ab3f733b4",
+    "url": "/hoi4/static/js/runtime~main.8bdac3c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hoi4/static/media/logo.5d5d9eef.svg"
+  }
+]);
