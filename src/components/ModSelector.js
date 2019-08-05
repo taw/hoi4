a32150bbf74b9ad48d96d2d3f077b764913bd870
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-08-05 17:50:32 +0100

    1.7.1 update

--- src/components/ModSelector.js
+++ src/components/ModSelector.js
@@ -5,8 +5,8 @@ export default class ModSelector extends Component {
     let {changeMod} = this.props;
     return <div className="mod-selector">
       <span>Switch mod:</span>
-      <button className="btn btn-primary" onClick={() => changeMod("vanilla")}>Vanilla (1.6.2)</button>
-      <button className="btn btn-primary" onClick={() => changeMod("kaiserreich")}>Kaiserreich (0.8.5 for 1.6.2)</button>
+      <button className="btn btn-primary" onClick={() => changeMod("vanilla")}>Vanilla (1.7.1)</button>
+      <button className="btn btn-primary" onClick={() => changeMod("kaiserreich")}>Kaiserreich (0.9.3 for 1.7.1)</button>
       <button className="btn btn-primary" onClick={() => changeMod("millennium_dawn")}>Millennium Dawn (0.3.0 for 1.5.4)</button>
     </div>
   }
