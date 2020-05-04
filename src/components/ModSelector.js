0334f1ba1dffc9c74f001d28213c46799c0769c7
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-05-04 16:53:27 +0100

    Update KR

--- src/components/ModSelector.js
+++ src/components/ModSelector.js
@@ -6,7 +6,7 @@ export default class ModSelector extends Component {
     return <div className="mod-selector">
       <span>Switch mod:</span>
       <button className="btn btn-primary" onClick={() => changeMod("vanilla")}>Vanilla (1.9.1)</button>
-      <button className="btn btn-primary" onClick={() => changeMod("kaiserreich")}>Kaiserreich (0.9.3 for 1.7.1)</button>
+      <button className="btn btn-primary" onClick={() => changeMod("kaiserreich")}>Kaiserreich (1.9.1)</button>
       <button className="btn btn-primary" onClick={() => changeMod("millennium_dawn")}>Millennium Dawn (0.3.0 for 1.5.4)</button>
     </div>
   }
